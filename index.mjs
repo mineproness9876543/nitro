cca2eb33c45fa7bb42c2c2f4abd2b2ea6835c1eb
mineproness9876543 <ben152393@gmail.com>
2025-05-23 11:29:27 +0600

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -4,7 +4,7 @@ import express from 'express'
 
 const app = express()
 function generateChar() {
-    const char = "a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 10".split(" ").join("")
+    const char = "a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split(" ").join("")
     let res = "";
     for (let a = 0; a < 19; a++) {
         res += char.charAt(Math.floor(Math.random() * char.length))
